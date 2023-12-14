f136ef60afe3a498d52dcc0646c17d42f8cadb0c
pwh-pwh <plmoknpwh@163.com>
2023-12-14 17:36:33 +0800

    change wss url2

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-var socket = new WebSocket("wss://coderpwh.cloudns.biz/ws")
+var socket = new WebSocket("wss://coderpwhdemo.zeabur.app/ws")
 
 let connect = cb => {
     console.log('Attempting Connection...');
